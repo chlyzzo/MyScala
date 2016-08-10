6d131deaa6c720c16e8ede013e2c76f950a6e858
wenhaohu <wenhaohu@anjuke.com>
2016-08-10 13:58:50 +0800

    add

--- src/readOther/readRabbitmq.scala
+++ src/readOther/readRabbitmq.scala
@@ -12,17 +12,17 @@ import org.slf4j.LoggerFactory
 object readRabbitmq {
   
     def createPropStream(ssc: StreamingContext): DStream[String] = {
-       val host = "10.249.6.26"
-       val quene = "bi_entrust_prop_updown"
+       val host = ""
+       val quene = ""
        val port =1750
-       val user = "nydus"
-       val pass = "nydus"
+       val user = ""
+       val pass = ""
       RabbitMQUtils.createStreamFromAQueue(ssc, host,  port, quene,StorageLevel.MEMORY_AND_DISK_SER_2,Option("/"), Option(user), Option(pass))
   }
     
    def main(args:Array[String]):Unit={
      println("************")
-     val conf = new SparkConf().setAppName("SkProRecomm").setIfMissing("spark.master", "local[4]")
+     val conf = new SparkConf().setAppName("").setIfMissing("spark.master", "local[4]")
      val ssc = new StreamingContext(conf, Seconds(2))
      val rdd=createPropStream(ssc: StreamingContext)
      rdd.print()
